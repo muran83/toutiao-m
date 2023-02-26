055e0be39b9f7ba9cd3f3b13706ac293944f3803
muran83 <1607783410@qq.com>
2023-02-26 11:34:47 +0800

    d3底部导航栏的构建

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,33 @@ const routes = [
     path: '/login',
     name: 'login',
     component: () => import('@/views/login') // 项目懒加载的写法
+  },
+  {
+    path: '/',
+    name: 'layout',
+    component: () => import('@/views/layout'),
+    children: [
+      {
+        path: '', // 默认子路由，只能有一个，当访问/的时候，显示home组件
+        name: 'home',
+        component: () => import('@/views/home')
+      },
+      {
+        path: '/qa',
+        name: 'qa',
+        component: () => import('@/views/qa')
+      },
+      {
+        path: '/video',
+        name: 'video',
+        component: () => import('@/views/video')
+      },
+      {
+        path: '/my',
+        name: 'my',
+        component: () => import('@/views/my')
+      }
+    ]
   }
 ]
 
---
+++ src/views/layout/index.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="layout-container">
+      <!-- 子路由出口 -->
+      <router-view />
+      <!-- /子路由出口 -->
+
+      <!-- 标签导航栏 -->
+      <!--
+        route: 开启路由模式，路由模式下会匹配路径和标签的to属性，并自动选中标签
+       -->
+      <van-tabbar class="layout-tabbar" route>
+        <van-tabbar-item to="/">
+          <i slot="icon" class="toutiao toutiao-shouye"></i>
+          <span class="text">首页</span>
+        </van-tabbar-item>
+        <van-tabbar-item to="/qa">
+          <i slot="icon" class="toutiao toutiao-wenda"></i>
+          <span class="text">问答</span>
+        </van-tabbar-item>
+        <van-tabbar-item to="/video">
+          <i slot="icon" class="toutiao toutiao-shipin"></i>
+          <span class="text">视频</span>
+        </van-tabbar-item>
+        <van-tabbar-item to="/my">
+          <i slot="icon" class="toutiao toutiao-wode"></i>
+          <span class="text">我的</span>
+        </van-tabbar-item>
+      </van-tabbar>
+      <!-- /标签导航栏 -->
+    </div>
+  </template>
+
+<script>
+export default {
+  name: 'LayoutIndex',
+  components: {},
+  props: {},
+  data () {
+    return {
+    }
+  },
+  computed: {},
+  watch: {},
+  created () {},
+  mounted () {},
+  methods: {}
+}
+</script>
+
+  <style scoped lang="less">
+  .layout-container {
+    .layout-tabbar {
+      i.toutiao {
+        font-size: 40px;
+      }
+      span.text {
+        font-size: 20px;
+      }
+    }
+  }
+  </style>
