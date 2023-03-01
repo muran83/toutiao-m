83fc3a5a8a06be6abab73dbec508a92865b7776d
muran83 <1607783410@qq.com>
2023-03-01 14:50:32 +0800

    时间模块处理前

--- src/components/article-item/index.vue
+++ src/components/article-item/index.vue
@@ -1,5 +1,41 @@
-<template>
-    <div class="article-item">文章列表项</div>
+  <template>
+    <van-cell 
+    class="article-item"
+    >
+      <div slot="title" class="title van-multi-ellipsis--l2">{{ article.title }}</div>
+      <div slot="label">
+        <!-- 三张图片的显示情况 -->
+        <div v-if="article.cover.type === 3 " class="cover-wrap">
+          <div class="cover-item" v-for="(imgSrc, index) in article.cover.images" :key="index ">
+            <van-image
+            class="cover-item-img"
+            slot="default"
+            :src="imgSrc"
+            fit="cover"
+         />
+          </div>
+        </div>
+        <div class="label-info-wrap">
+          <span slot="label">{{ article.aut_name}}</span>
+          <span slot="label">{{ article.comm_count}}条评论</span>
+          <span slot="label">{{ article.pubdate}}</span>
+        </div>
+      </div>
+      <!-- 一张图片的情况 -->
+      <div 
+      class="right-cover" 
+      slot="default" 
+      v-if="article.cover.type === 1">
+        <van-image
+          fit="cover"
+          slot="default"
+          :src="article.cover.images[0]"
+         />
+      </div>
+
+  </van-cell>
+    
+    
   </template>
   
   <script>
@@ -23,4 +59,42 @@
   }
   </script>
   
-  <style scoped lang="less"></style>
+  <style scoped lang="less">
+  .article-item {
+    .title {
+      font-size: 32px;
+      color: #3a3a3a;
+    }
+
+    .van-cell__value {
+      flex: unset;
+      width: 232px;
+      height: 146px;
+      padding-left: 25px;
+    }
+    .right-cover {
+      width: 232px;
+      height: 146px;
+    }
+    .label-info-wrap span {
+      font-size: 22px;
+      color: #b4b4b4;
+      margin-right: 25px;
+    }
+    .cover-wrap {
+      padding: 30px 0;
+      display: flex;
+      .cover-item {
+        flex: 1;
+        height: 146px;
+        &:not(:last-child) {
+          padding-right: 4px;
+        }
+        .cover-item-img {
+          width: 100%;
+          height: 146px;
+        }
+      }
+    }
+  }
+  </style>
