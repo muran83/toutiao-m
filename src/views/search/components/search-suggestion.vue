b818babd2a984cae6aae5ed6cdfb678240024623
muran83 <1607783410@qq.com>
2023-03-02 20:32:46 +0800

    d5 防抖的完善

--- src/views/search/components/search-suggestion.vue
+++ src/views/search/components/search-suggestion.vue
@@ -28,9 +28,13 @@
     computed: {},
     watch: {
       searchText: {
-        handler (val) {
+        // handler 的另一种写法
+        handler: debounce (function(val) {
           this.loadSearchSuggestions(val)
-        },
+        }, 300),
+        // handler (val) {
+        //   this.loadSearchSuggestions(val)
+        // }
         immediate: true // 首次监视触发 ，第一次往输入框输入一个字符的时候才渲染出搜索建议组件然后才调用，immediate可以立即调用
       }
     },
